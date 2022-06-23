76f838ee1ad8f4cb70643499f82e6b1f45bbc0e9
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-23 11:31:55 +0530

    Add multiple People

---
+++ Address_book/Address_book/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace AdressBook
+{
+    class program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AdressBook");
+            AdressBook address = new AdressBook();
+
+            string command = "";
+
+            while (command != "exit")
+            {
+
+                Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+                command = Console.ReadLine().ToLower();
+
+                switch (command)
+                {
+                    case "add":
+
+
+                        address.AddPerson();
+                        break;
+                    case "edit":
+                        address.EditContact();
+                        break;
+                    case "remove":
+                        address.RemoveContact();
+                        break;
+                    case "display":
+                        address.Display();
+                        break;
+                    case "multipleperson":
+                        address.AddMultipleContact();
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
